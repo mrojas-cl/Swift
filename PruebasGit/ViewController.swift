ca2493c4b44b326f8e57c3054b5320eeb9c485b8
Marco Rojas Bertolone <mrojas@macbook-pro-de-marco.local>
2016-04-19 12:28:20 -0300

    Segunda

--- PruebasGit/ViewController.swift
+++ PruebasGit/ViewController.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    // Prueba de GitHub
-
+    // Segunda Prueba
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
